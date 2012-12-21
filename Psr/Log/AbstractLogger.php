ab214e4da4a0194988f762040c4b0ac9bf6d8290
Jordi Boggiano <j.boggiano@seld.be>
2012-12-21 09:57:53 +0100

    Add README

--- Psr/Log/AbstractLogger.php
+++ Psr/Log/AbstractLogger.php
@@ -3,10 +3,10 @@
 namespace Psr\Log;
 
 /**
- * This is a simple Logger implementation that other Loggers can inherit from. 
+ * This is a simple Logger implementation that other Loggers can inherit from.
  *
- * It simply delegates all log-level-specific methods to the `log` method to 
- * reduce boilerplate code that a simple Logger that does the same thing with 
+ * It simply delegates all log-level-specific methods to the `log` method to
+ * reduce boilerplate code that a simple Logger that does the same thing with
  * messages regardless of the error level has to implement.
  */
 abstract class AbstractLogger implements LoggerInterface
