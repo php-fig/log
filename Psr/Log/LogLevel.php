05985c33d3e0e4e3b60e80434f87a6c56b8c891d
Javier Aguilar <javier@anid.es>
2013-01-24 00:40:26 +0100

    Possible solution to issue php-fig/log#8 , based on Syslog standard

--- Psr/Log/LogLevel.php
+++ Psr/Log/LogLevel.php
@@ -7,12 +7,42 @@ namespace Psr\Log;
  */
 class LogLevel
 {
-    const EMERGENCY = 'emergency';
-    const ALERT = 'alert';
-    const CRITICAL = 'critical';
-    const ERROR = 'error';
-    const WARNING = 'warning';
-    const NOTICE = 'notice';
-    const INFO = 'info';
-    const DEBUG = 'debug';
+
+    const EMERGENCY = 0;
+    const ALERT = 1;
+    const CRITICAL = 2;
+    const ERROR = 3;
+    const WARNING = 4;
+    const NOTICE = 5;
+    const INFO = 6;
+    const DEBUG = 7;
+
+    /**
+     * Level names
+     * @var array
+     */
+    protected static $names = array(
+        0 => "emergency",
+        1 => "alert",
+        2 => "critical",
+        3 => "error",
+        4 => "warning",
+        5 => "notice",
+        6 => "info",
+        7 => "debug"
+    );
+
+    /**
+     * Returns a level name
+     * @param int $level
+     * @return string|null The level name or null if the level doesn't exist
+     */
+    public static function getName($level)
+    {
+        if (isset(static::$names[$level]))
+            return static::$names[$level];
+        else
+            return null;
+    }
+
 }
