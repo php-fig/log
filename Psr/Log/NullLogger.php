b292041a15ca49b691f0e28e8e1c0f8a0a477f4a
Ayhan Akilli <info@ayhan.biz>
2015-07-20 10:09:14 +0200

    Forgot to omit the "@return null" tag in NullLogger

--- Psr/Log/NullLogger.php
+++ Psr/Log/NullLogger.php
@@ -18,8 +18,6 @@ class NullLogger extends AbstractLogger
      * @param mixed  $level
      * @param string $message
      * @param array  $context
-     *
-     * @return null
      */
     public function log($level, $message, array $context = array())
     {
