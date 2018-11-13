d006e6b8358f7a2889a40bb4072636c978803fb5
Mponos George <gmponos@gmail.com>
2018-11-13 23:56:24 +0200

    Add a public property to LogLevel containing the levels

--- Psr/Log/LogLevel.php
+++ Psr/Log/LogLevel.php
@@ -15,4 +15,15 @@ class LogLevel
     const NOTICE    = 'notice';
     const INFO      = 'info';
     const DEBUG     = 'debug';
+    
+    public static $levels = [
+        self::EMERGENCY => self::EMERGENCY,
+        self::ALERT     => self::ALERT,
+        self::CRITICAL  => self::CRITICAL,
+        self::ERROR     => self::ERROR,
+        self::WARNING   => self::WARNING,
+        self::NOTICE    => self::NOTICE,
+        self::INFO      => self::INFO,
+        self::DEBUG     => self::DEBUG,
+    ];
 }
