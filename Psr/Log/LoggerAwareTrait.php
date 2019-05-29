299fbd06f2a44276ec0e2f6af1f2ce6e98a24d2e
Slayer911 <slayer911@inbox.ru>
2019-05-29 11:58:18 +0300

    Add logger getter in LoggerAwareTrait
    if you like this changes, also we need change "LoggerAwareInterface"

--- Psr/Log/LoggerAwareTrait.php
+++ Psr/Log/LoggerAwareTrait.php
@@ -23,4 +23,20 @@ trait LoggerAwareTrait
     {
         $this->logger = $logger;
     }
+
+    /**
+     * Get a logger
+     * Always return LoggerInterface instance
+     * is a way to avoid littering your code with `if ($this->logger) { }`
+     *
+     * @return LoggerInterface
+     */
+    public function getLogger()
+    {
+        if (is_null($this->logger)) {
+            $this->logger = new NullLogger();
+        }
+
+        return $this->logger;
+    }
 }
